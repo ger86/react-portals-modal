9512a6568ed0ca442e13c361b72fa5f6b897af1f
Gerardo Fernández <gerardo@latteandcode.com>
2019-12-18 10:44:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 import Modal from "./Modal";
 import "./App.css";
 
+function Child() {
+  return <button>Click</button>;
+}
+
 function App() {
   const [isOpened, setOpened] = useState(false);
+  const [counter, setCounter] = useState(0);
 
   const openModal = () => setOpened(true);
   const closeModal = () => setOpened(false);
+  const handleClick = () => setCounter(counter + 1);
   return (
-    <div className="App">
+    <div className="App" onClick={handleClick}>
       <header className="App-header">
         <button className="btn btn-primary" onClick={openModal}>
           Abrir modal
         </button>
-        <Modal title="Welcome" isOpened={isOpened} onClose={closeModal}>This is a modal</Modal>
+        <div>Counter: {counter}</div>
+        <Modal title="Welcome" isOpened={isOpened} onClose={closeModal}>
+          <Child />
+        </Modal>
       </header>
     </div>
   );
