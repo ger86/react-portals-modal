dde977beec8774ddf46de8e2c85d69a146601bbb
Gerardo Fernández <gefe.86@gmail.com>
2020-01-18 12:32:49 +0100

    added show animation

--- src/Modal.js
+++ src/Modal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 import "./Modal.css";
 
 const modalContainer = document.querySelector("#modalContainer");
@@ -8,17 +9,25 @@ export default ({ title, children, isOpened, onClose }) =>
   isOpened
     ? ReactDOM.createPortal(
         <div className="modal" tabIndex="-1" role="dialog">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">{title}</h5>
-                <button onClick={onClose}>
-                  <span aria-hidden="true">&times;</span>
-                </button>
+          <CSSTransition
+            appear
+            in
+            classNames="modal-transition"
+            unmountOnExit
+            timeout={300}
+          >
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">{title}</h5>
+                  <button onClick={onClose}>
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">{children}</div>
               </div>
-              <div className="modal-body">{children}</div>
             </div>
-          </div>
+          </CSSTransition>
         </div>,
         modalContainer
       )
